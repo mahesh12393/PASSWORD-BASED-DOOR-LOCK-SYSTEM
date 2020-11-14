b140fb79c43c7ef3de5780a0571530e2128ad11d
karan karhale <karanp.karhale@gmail.com>
2020-11-14 21:26:33 +0530

    Simulation and code of buzzer is updated!

--- doorlockcode/doorcode.c
+++ doorlockcode/doorcode.c
@@ -14,23 +14,26 @@ sbit RS = P3^0;
 sbit EN = P3^1;
 //Motor connections
 sbit IN1 =P3^2;
-sbit IN2 = P3^3;
+sbit IN2 =P3^3;
+sbit IN3 =P3^4;
+sbit IN4 =P3^5;
 //user defined functions
 void lcdcmd(unsigned char); //LCD commands
 void lcddat(unsigned char); 
 void lcddisplay(unsigned char*); //LCD display
 unsigned char password[5]; 
+int x=0;
+int beep=0;
 unsigned char correctpassword[5]="02349"; //this is the correct password
 void delay(int);
 void check();
 void delay (int a){
     int i,j;
     for(i=0;i<a;i++){
-        for(j=0;j<255;j++);
+        for(j=0;j<1275;j++);
     }
 }
-void main(){	 
-	  int x=0;
+void lcdstart(){
     lcdcmd(0x01); //clear screen
 	  lcdcmd(0x38); // 5*7 matrix
 	  lcdcmd(0x0E); //display on, cursor blinking
@@ -38,30 +41,33 @@ void main(){
     lcddisplay("DOOR LOCK");
     lcdcmd(0xc0); //force cursor to beginning of second line
     lcddisplay("   SYSTEM");
-	  delay(10);
+	  delay(1);
 	  lcdcmd(0x01); //clear screen
 	  lcddisplay("Enter Password");
-	  lcdcmd(0xc0); //force cursor to beginning of second line
-    while(1){        
+	  lcdcmd(0xc0); //force cursor to beginning of second line	
+}
+void solve(){
+	  lcdstart();
+	  while(1){        
         r1=0;r2=1;r3=1;r4=1;        
         if(c1==0){            
             password[x]='1';
             lcddat('*');  
-					  delay(200);
+					  delay(100);
 					  lcdcmd(0x06);     //shift cursor to right        
             x++;
         }
         if(c2==0){
             password[x]='2';
             lcddat('*');  
-					  delay(200);
+					  delay(100);
 					  lcdcmd(0x06);     //shift cursor to right  
             x++;
         }
         if(c3==0){
             password[x]='3';
             lcddat('*');  
-					  delay(200);
+					  delay(100);
 					  lcdcmd(0x06);      //shift cursor to right      
             x++;
         }
@@ -69,21 +75,21 @@ void main(){
         if(c1==0){
             password[x]='4';
             lcddat('*');  
-					  delay(200);
+					  delay(100);
 					  lcdcmd(0x06);      //shift cursor to right      
             x++;
         }
         if(c2==0){
             password[x]='5';
             lcddat('*');  
-					  delay(200);
+					  delay(100);
 					  lcdcmd(0x06);      //shift cursor to right      
             x++;
         }
         if(c3==0){
             password[x]='6';
             lcddat('*');  
-					  delay(200);
+					  delay(100);
 					  lcdcmd(0x06);   //shift cursor to right         
             x++;
         }
@@ -91,21 +97,21 @@ void main(){
         if(c1==0){
             password[x]='7';
             lcddat('*');  
-					  delay(200);
+					  delay(100);
 					  lcdcmd(0x06);       //shift cursor to right     
             x++;
         }
         if(c2==0){
             password[x]='8';
             lcddat('*');  
-					  delay(200);
+					  delay(100);
 					  lcdcmd(0x06);       //shift cursor to right     
             x++;
         }
         if(c3==0){
             password[x]='9';
             lcddat('*');  
-					  delay(200);
+					  delay(100);
 					  lcdcmd(0x06);      //shift cursor to right      
             x++;
         }
@@ -113,17 +119,20 @@ void main(){
         if(c2==0){
             password[x]='0';
             lcddat('*');  
-					  delay(200);
+					  delay(100);
 					  lcdcmd(0x06);         //shift cursor to right   
             x++;
         }
         if(x==5){
             check();
-            delay(200);
+            delay(100);
 					  lcdcmd(0x06);       //shift cursor to right
         }
         r4=1;
     }
+}
+void main(){
+	  solve();
 }
 void check(){
     if(password[0]==correctpassword[0]&&
@@ -138,7 +147,11 @@ void check(){
            lcddisplay("Password");
            lcdcmd(0xc0);
            lcddisplay("Matched");
-           delay(1000);
+           delay(500);
+				   IN1=0;
+				   IN2=0;
+				   x=0;
+				   solve();
        }
        else{                                        //if password doesnt match
            IN1=0;
@@ -147,8 +160,17 @@ void check(){
            lcdcmd(0x80);
            lcddisplay("Wrong");
            lcdcmd(0xc0);
-           lcddisplay("Password");
-           delay(100);
+           lcddisplay("Password");				   
+				   for(beep=0;beep<3;beep++){						 
+				   IN3=1;
+				   IN4=0;
+           delay(200);
+				   IN3=0;
+				   IN4=0;
+				   delay(10);
+					 }
+				   x=0;
+				   solve();
        }
 }
 void lcdcmd(unsigned char cmd){
